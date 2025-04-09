258a9ddca3e554296cb6ddd26b558530bc47661b
truong965 <22714791.truong@student.iuh.edu.vn>
2025-04-09 17:06:30 +0700

    6. React Router

--- ThucHanh2_4/lab5/src/App.jsx
+++ ThucHanh2_4/lab5/src/App.jsx
@@ -11,12 +11,18 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Lab_05 />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="project" element={<Project />} />
-            <Route path="theme" element={<Theme />} />
-            <Route path="analytics" element={<Analytics />} />
-            <Route path="messages" element={<Messages />} />
-            <Route path="integrations" element={<Integrations />} />
+            <Route path="dashboard"
+              element={<Dashboard />} />
+            <Route path="project"
+              element={<Project />} />
+            <Route path="theme"
+              element={<Theme />} />
+            <Route path="analytics"
+              element={<Analytics />} />
+            <Route path="messages"
+              element={<Messages />} />
+            <Route path="integrations"
+              element={<Integrations />} />
           </Route>
         </Routes>
       </Router >
